cef3d4a9935ea1ed651484a42169074a524db7b9
purple <purple29b@gmail.com>
2022-01-25 14:18:07 +0900

    lombok

--- chatProject/src/main/java/com/project/chat/chatting/ChatContentDTO.java
+++ chatProject/src/main/java/com/project/chat/chatting/ChatContentDTO.java
@@ -1,7 +1,10 @@
 package com.project.chat.chatting;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class ChatContentDTO {
 	
 	int num;
@@ -10,43 +13,5 @@ public class ChatContentDTO {
 	String content;
 	String thumnailPath;
 	Date regDate;
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public int getRoomNum() {
-		return roomNum;
-	}
-	public void setRoomNum(int roomNum) {
-		this.roomNum = roomNum;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	
-	public String getThumnailPath() {
-		return thumnailPath;
-	}
-	public void setThumnailPath(String thumnailPath) {
-		this.thumnailPath = thumnailPath;
-	}
-	public Date getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-	
 	
 }
--- chatProject/src/main/java/com/project/chat/user/UserVO.java
+++ chatProject/src/main/java/com/project/chat/user/UserVO.java
@@ -1,5 +1,8 @@
 package com.project.chat.user;
 
+import lombok.Data;
+
+@Data
 public class UserVO {
 	
 	String id;
@@ -7,36 +10,7 @@ public class UserVO {
 	String thumbnailPath;
 	String stateMessage;
 	String chatRooms;
-	public String getChatRooms() {
-		return chatRooms;
-	}
-	public void setChatRooms(String chatRooms) {
-		this.chatRooms = chatRooms;
-	}
-	public String getStateMessage() {
-		return stateMessage;
-	}
-	public void setStateMessage(String stateMessage) {
-		this.stateMessage = stateMessage;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getThumbnailPath() {
-		return thumbnailPath;
-	}
-	public void setThumbnailPath(String thumbnailPath) {
-		this.thumbnailPath = thumbnailPath;
-	}
+
 	
 	@Override
 	public String toString() {
